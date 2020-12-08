3cc28dfe0236cbdc154277fbd1be800f128a8991
Hamayun Aziz <hamayunaziz@codonist.com>
2020-12-08 17:14:59 +0500

    updates

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    jQuery( "body" ).buoyant({
+        numberOfItems: 15,
+        minRadius: 5,
+        maxRadius: 25,
+        minSpeed: 10,
+        maxSpeed: 100,
+        elementClass: 'circles'
+    });
+});// End of document ready function
