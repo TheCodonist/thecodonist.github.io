155e6696111381e51d2d4499b5959f67463d5695
Hamayun Aziz <hamayunaziz@codonist.com>
2020-12-19 15:21:57 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
-    jQuery( "body" ).buoyant({
-        numberOfItems: 15,
-        minRadius: 5,
-        maxRadius: 25,
-        minSpeed: 10,
-        maxSpeed: 100,
-        elementClass: 'circles'
-    });
-});// End of document ready function
+// $(document).ready(function(){
+//     jQuery( "body" ).buoyant({
+//         numberOfItems: 15,
+//         minRadius: 5,
+//         maxRadius: 25,
+//         minSpeed: 10,
+//         maxSpeed: 100,
+//         elementClass: 'circles'
+//     });
+// });
+       
