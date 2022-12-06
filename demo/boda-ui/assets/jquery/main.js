ba23d71adb2884e07ad354bd534375d3f4faa9f1
Hamayun Aziz <hamayunaziz@codonist.com>
2022-12-06 23:00:06 +0500

    boda

---
+++ demo/boda-ui/assets/jquery/main.js
@@ -0,0 +1,15 @@
+var $container = $('.step');
+var $p = $container.find('p');
+var currentIndex = 0;
+
+$p.on('click', function (e) {
+  var $current = $(e.currentTarget);
+  var index = $p.index($current);
+  if (index > currentIndex) {
+  	$container.addClass('forward');
+  } else {
+    $container.removeClass('forward');
+  }
+  currentIndex = index;
+  $container.attr('data-step', index);
+});
